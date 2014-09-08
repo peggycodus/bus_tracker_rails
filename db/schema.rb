46749b66178278e9f631289971f0e655db90f2e3
Peggy Condon <peggyc3@gmail.com>
2014-09-08 10:33:01 -0700

    added lines

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140908172947) do
+ActiveRecord::Schema.define(version: 20140908173137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "lines", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "stops", force: true do |t|
     t.string   "name"
